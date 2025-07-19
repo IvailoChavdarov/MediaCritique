a626493e8e9dc2408fd094a240d7ce002c25d5e9
Ivailo Chavdarov <ivailochav@gmail.com>
2025-07-19 17:54:41 +0300

    Updated frequent lies list page banner component and updated responsive design

--- src/components/FrequentLiesBanner/FrequentLiesBanner.js
+++ src/components/FrequentLiesBanner/FrequentLiesBanner.js
@@ -1,44 +1,49 @@
 import './FrequentLiesBanner.scss'
 import brics from './images/brics.jpg'
 import covid from './images/covid.jpg'
-import eu from './images/eu.jpg'
+import eu from './images/eu.png'
 import euro from './images/euro.jpg'
 import nato from './images/nato.jpg'
 import putin from './images/putin.jpg'
-import shengen from './images/shengen.jpg'
 import zhivkov from './images/zhivkov.jpg'
+import shengen from './images/shengen.jpg'
+import lgbt from './images/lgbt.jpg'
 export default function FrequentLiesBanner(){
     return(
         <div className='lies-banner'>
             <div className="banner-background">
-                <div className='div-1'>
-                    <img src={brics} alt='BRICS'/>
-                </div>
-                <div className='div-2'>
-                    <img src={euro} alt='Euro'/>
-                </div>
-                <div className='div-3'>
+                <div>
                     <img src={zhivkov} alt='Todor Zhivkov'/>
                 </div>
-                <div className='div-4'>
+                <div>
+                    <img src={lgbt} alt='LGBT'/>
+                </div>
+                <div>
                     <img src={covid} alt='Covid-19'/>
                 </div>
-                <div className='div-5'>
-                    <img src={putin} alt='Russia'/>
-                </div>
-                <div className='div-6'>
-                    <img src={nato} alt='NATO'/>
-                </div>
-                <div className='div-7'>
+                <div>
+
                     <img src={eu} alt='European/Soviet Union'/>
                 </div>
-                <div className='div-8'>
+                <div>
+                    <img src={putin} alt='Russia'/>
+                </div>
+                <div>
+                    <img src={nato} alt='NATO'/>
+                </div>
+                <div>
+                    <img src={euro} alt='Euro'/>
+                </div>
+                <div>
                     <img src={shengen} alt='Shengen'/>
+                </div>
+                <div>
+                    <img src={brics} alt='BRICS'/>
                 </div>
             </div>
             <div className='banner-titles'>
                 <h1>ЧЕСТО СРЕЩАНИ ЛЪЖИ В БЪЛГАРСКИТЕ МЕДИИ</h1>
-                <h2>Разобличаваме дезинформацията, която подкопава демокрацията, подменя истината и руши доверието в институциите.</h2>
+                <h2>Разобличаваме дезинформацията, която подкопава демокрацията, подменя истината и забавя изграждането на гражданското общество.</h2>
             </div>
         </div>
     )
